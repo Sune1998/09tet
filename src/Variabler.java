f6af664588d444d3c0409ac8fa12b101141200bf
Sune1998 <53908735+Sune1998@users.noreply.github.com>
2019-09-09 09:47:16 +0200

    Initial commit

---
+++ src/Variabler.java
@@ -0,0 +1,31 @@
+public class Variabler {
+
+    public static void main(String[] args) {
+        int a = 1023;
+        int b = 10024;
+        short c = 1001;
+        byte d = 123;
+
+        double e = 2.7;
+        float f = 5.0f;
+
+        boolean  g = true;
+        boolean h = false;
+
+        String i = "pølse";
+
+        char j = 'b';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+        
+    }
+}
